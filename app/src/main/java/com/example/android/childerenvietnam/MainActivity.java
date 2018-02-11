5ec74ab8e6a4388547f64ba1a1b4e842c5da3e32
nanne <nannestephan@gmail.com>
2018-02-11 17:49:21 +0100

    FireBase start Email/password Dialog account Creation

--- app/src/main/java/com/example/android/childerenvietnam/MainActivity.java
+++ app/src/main/java/com/example/android/childerenvietnam/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.android.childerenvietnam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+
+
+
 }
+
